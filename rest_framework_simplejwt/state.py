8674d2d570063e2349b4569381762500c5ff77c3
Richard Jansen <richard@pedroja.tech>
2021-03-26 21:32:55 +0100

    retreive a new instanceof TokenBackend on usage, so audience is set correctly each time to the tenants schema

--- rest_framework_simplejwt/state.py
+++ rest_framework_simplejwt/state.py
@@ -1,5 +1,9 @@
 from .backends import TokenBackend
 from .settings import api_settings
 
-token_backend = TokenBackend(api_settings.ALGORITHM, api_settings.SIGNING_KEY,
-                             api_settings.VERIFYING_KEY, api_settings.AUDIENCE, api_settings.ISSUER)
+from django.db import connection
+
+
+def get_token_backend():
+	return TokenBackend(api_settings.ALGORITHM, api_settings.SIGNING_KEY,
+                        api_settings.VERIFYING_KEY, connection.tenant.schema_name, api_settings.ISSUER)
